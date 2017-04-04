05b761bc1c9eda079b33e55648e6d2923a9ece55
Chad Myers <chad.myers@gmail.com>
2017-04-04 15:56:28 -0500

    Convert to DT build format

--- rakefile.rb
+++ rakefile.rb
@@ -1,139 +1,63 @@
-COMPILE_TARGET = ENV['config'].nil? ? "Debug" : ENV['config'] # Keep this in sync w/ VS settings since Mono is case-sensitive
-CLR_TOOLS_VERSION = "v4.0.30319"
-
-buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
-
-if( ! buildsupportfiles.any? )
-  # no buildsupport, let's go get it for them.
-  sh 'git submodule update --init' unless buildsupportfiles.any?
-  buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
-end
-
-# nope, we still don't have buildsupport. Something went wrong.
-raise "Run `git submodule update --init` to populate your buildsupport folder." unless buildsupportfiles.any?
-
-buildsupportfiles.each { |ext| load ext }
-
-include FileTest
-require 'albacore'
-gem 'rubyzip'
-require 'zip/zip'
-require 'zip/zipfilesystem'
-load "VERSION.txt"
-
-RESULTS_DIR = "results"
-PRODUCT = "Milkman"
-COPYRIGHT = 'Copyright 2008-2013 Dru Sellers, Jeremy D. Miller, et al. All rights reserved.';
-COMMON_ASSEMBLY_INFO = 'src/CommonAssemblyInfo.cs';
-BUILD_DIR = File.expand_path("build")
-ARTIFACTS = File.expand_path("artifacts")
-
-@teamcity_build_id = "bt24"
+COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
+BUILD_VERSION = '100.0.0'
 tc_build_number = ENV["BUILD_NUMBER"]
 build_revision = tc_build_number || Time.new.strftime('5%H%M')
-BUILD_NUMBER = "#{BUILD_VERSION}.#{build_revision}"
+build_number = "#{BUILD_VERSION}.#{build_revision}"
+BUILD_NUMBER = build_number 
 
-props = { :stage => BUILD_DIR, :artifacts => ARTIFACTS }
+desc 'Compile the code'
+task :compile => [:clean, :version] do
+  msbuild = '"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"'
+  sh "#{msbuild} src/Milkman.sln /property:Configuration=#{COMPILE_TARGET} /v:m /t:rebuild /nr:False /maxcpucount:8"
+end
 
-desc "**Default**, compiles and runs tests"
-task :default => [:compile, :create_bottles, :unit_test]
+desc 'Run the unit tests'
+task :test => [:compile, :fast_test] do
+end
 
-desc "Full compile and test (same as 'default' task)"
-task :full => [:default]
+desc 'Run the unit tests without compile'
+task :fast_test do
+  sh "src/packages/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe src/Milkman.Testing/bin/#{COMPILE_TARGET}/Milkman.Testing.dll"
+end
 
-desc "Target used for the CI server"
-task :ci => [:update_all_dependencies, :default, :history, :package]
-
-desc "Target used for CI on Mono"
-task :mono_ci => [:update_all_dependencies, :default, :mono_unit_test]
+desc "Prepares the working directory for a new build"
+task :clean do
+  FileUtils.rm_rf 'artifacts'
+  Dir.mkdir 'artifacts'
+end
 
 desc "Update the version information for the build"
-assemblyinfo :version do |asm|
-  asm_version = BUILD_NUMBER
-
+task :version do
+  asm_version = build_number
+  
   begin
     commit = `git log -1 --pretty=format:%H`
   rescue
     commit = "git unavailable"
   end
-  puts "##teamcity[buildNumber '#{BUILD_NUMBER}']" unless tc_build_number.nil?
-  puts "Version: #{BUILD_NUMBER}" if tc_build_number.nil?
-  asm.trademark = commit
-  asm.product_name = PRODUCT
-  asm.description = BUILD_NUMBER
-  asm.version = asm_version
-  asm.file_version = BUILD_NUMBER
-  asm.custom_attributes :AssemblyInformationalVersion => asm_version
-  asm.copyright = COPYRIGHT
-  asm.output_file = COMMON_ASSEMBLY_INFO
-end
-
-desc "Prepares the working directory for a new build"
-task :clean => [:update_buildsupport] do
-
-  FileUtils.rm_rf props[:stage]
-    # work around nasty latency issue where folder still exists for a short while after it is removed
-    waitfor { !exists?(props[:stage]) }
-  Dir.mkdir props[:stage]
-
-  Dir.mkdir props[:artifacts] unless exists?(props[:artifacts])
-end
-
-def waitfor(&block)
-  checks = 0
-  until block.call || checks >10
-    sleep 0.5
-    checks += 1
+  puts "##teamcity[buildNumber '#{build_number}']" unless tc_build_number.nil?
+  puts "Version: #{build_number}" if tc_build_number.nil?
+  
+  options = {
+    :description => 'Milkman',
+    :product_name => 'Milkman',
+    :copyright => 'Copyright 2008-2013 Dru Sellers, Jeremy D. Miller, et al. All rights reserved.',
+    :trademark => commit,
+    :version => asm_version,
+    :file_version => build_number,
+    :informational_version => asm_version
+  }
+  
+  puts "Writing src/CommonAssemblyInfo.cs..."
+  File.open('src/CommonAssemblyInfo.cs', 'w') do |file|
+    file.write "using System.Reflection;\n"
+    file.write "using System.Runtime.InteropServices;\n"
+    file.write "[assembly: AssemblyDescription(\"#{options[:description]}\")]\n"
+    file.write "[assembly: AssemblyProduct(\"#{options[:product_name]}\")]\n"
+    file.write "[assembly: AssemblyCopyright(\"#{options[:copyright]}\")]\n"
+    file.write "[assembly: AssemblyTrademark(\"#{options[:trademark]}\")]\n"
+    file.write "[assembly: AssemblyVersion(\"#{options[:version]}\")]\n"
+    file.write "[assembly: AssemblyFileVersion(\"#{options[:file_version]}\")]\n"
+    file.write "[assembly: AssemblyInformationalVersion(\"#{options[:informational_version]}\")]\n"
   end
-  raise 'waitfor timeout expired' if checks > 10
 end
-
-desc "Compiles the app"
-task :compile => [:restore_if_missing, :clean, :version, "docs:bottle"] do
-  MSBuildRunner.compile :compilemode => COMPILE_TARGET, :solutionfile => 'src/Milkman.sln', :clrversion => CLR_TOOLS_VERSION
-  target = COMPILE_TARGET.downcase
-end
-
-task :create_bottles => :compile do
-  milk_dir = "src/milk/bin/#{COMPILE_TARGET}"
-  
-  sh "#{milk_dir}/milk.exe create-all --output build --target #{COMPILE_TARGET}"
-  File.delete "build/milkman.zip" if File.exist? "build/milkman.zip"
-  
-  outer_file_list = FileList.new("#{milk_dir}/*.*") do |fl|
-    fl.exclude("#{milk_dir}/*.xml")
-    fl.exclude("#{milk_dir}/*vshost*")    
-  end
-  
-  bin_folder_file_list = FileList.new("#{milk_dir}/*.dll", "#{milk_dir}/*.exe") do |fl|
-    fl.exclude("#{milk_dir}/*vshost*")
-    fl.exclude("#{milk_dir}/*Deployers*")    
-  end
-  
-  Zip::ZipFile.open("build/milkman.zip", Zip:ZipFile::CREATE) do |zip|
-    outer_file_list.to_a.each do |f|
-      zip.add(f.sub("#{milk_dir}/",''), f)
-    end
-    
-    admin = "#{milk_dir}/../../../packages/Microsoft.Web.Administration/lib/net20/Microsoft.Web.Administration.dll"
-    zip.add("Microsoft.Web.Administration.dll", admin)
-    
-    bin_folder_file_list.to_a.each do |f|
-      zip.add(f.sub("#{milk_dir}/",'bin/'), f)
-    end
-    zip.add("bin/Microsoft.Web.Administration.dll", admin)
-  end  
-end
-
-
-desc "Runs unit tests"
-task :test => [:unit_test]
-
-desc "Runs unit tests"
-task :unit_test => :compile do
-  runner = NUnitRunner.new :compilemode => COMPILE_TARGET, :source => 'src', :platform => 'x86'
-  runner.executeTests ['Milkman.Testing']
-end
-
-desc "Runs some of the unit tests for Mono"
-task :mono_unit_test => [:unit_test]
